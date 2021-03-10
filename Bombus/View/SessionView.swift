b81f9c593f33c10a32a3ebfe2fd4725b0585449a
Yon Montoto <yon.montoto@gmail.com>
2021-03-10 16:47:22 -0500

    implement basic timer functionality

--- Bombus/View/SessionView.swift
+++ Bombus/View/SessionView.swift
@@ -2,17 +2,33 @@ import SwiftUI
 
 struct SessionView: View {
 
+    @ObservedObject var viewModel: ViewModel
     let storageProvider: StorageProvider
 
     var body: some View {
-        NavigationView {
-            List(storageProvider.getAllSessions(), id: \.self) { session in
-                Text("Duration: \(session.duration)")
-                    .fontWeight(.semibold)
+            VStack {
+                // Let's place this into a ButtonStyleModifier so we dont clog the view
+                Text("\(viewModel.formatter.string(from: viewModel.timerDuration) ?? "Nothing")")
+                    .font(.system(size: 50, weight: .bold, design: .rounded))
+                    .fontWeight(.bold)
+                    .padding(50)
+
+                Spacer()
+
+                Button(action: {
+                    viewModel.startTimer()
+                }, label: {
+                    // Let's place this into a ButtonStyleModifier so we dont clog the view
+                    Text("Start Timer")
+                        .font(.system(.headline, design: .rounded))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: 50) // Hardcoded for now to see how it looks
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .padding()
+                })
             }
             .navigationTitle("Sessions")
-        }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -25,6 +41,6 @@ struct SessionView_Previews: PreviewProvider {
         dummySession.points = 2
         dummySession.start = Date.init(timeIntervalSinceNow: -10)
 
-        return SessionView(storageProvider: viewContext)
+        return SessionView(viewModel: SessionView.ViewModel(), storageProvider: viewContext)
     }
 }
