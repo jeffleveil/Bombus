9485646f95f4289211aeb8bcc2d909dfc8956152
Yon Montoto <yon.montoto@gmail.com>
2021-03-16 16:00:39 -0400

    add the removed tabview. oops

---
+++ Bombus/View/BombusTabView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct BombusTabView: View {
+    @State private var selection = 2
+    var body: some View {
+        TabView(selection: $selection) {
+            Text("Place Holder")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("")
+                }.tag(1)
+            Text("Place Holder")
+                .tabItem {
+                    Image(systemName: "stopwatch")
+                    Text("")
+                }.tag(2)
+            Text("Place Holder")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("")
+                }.tag(3)
+        }
+    }
+}
+
+struct BombusTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        BombusTabView()
+    }
+}
