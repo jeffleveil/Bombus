06078fae77ba185bf1b59da0805aa507de8d836d
Nicolas Rios <nickrios33@yahoo.com>
2021-03-16 12:57:16 -0700

    add color extension

---
+++ Bombus/View/Extensions/Color+Extension.swift
@@ -0,0 +1,12 @@
+
+import SwiftUI
+
+extension Color {
+    public static var bombusbBackgroundColor: Color {
+        return Color(UIColor(red: 255/255, green: 238/255, blue: 147/255, alpha: 1.0))
+    }
+    public static var bombusbAccentColor: Color {
+        return Color(UIColor(red: 245/255, green: 215/255, blue: 130/255, alpha: 1.0))
+    }
+
+}
