91ad1404ebacf673f7b9c96ad8500704e92fbf1c
Yon Montoto <yon.montoto@gmail.com>
2021-03-16 15:04:56 -0400

    create button modifier

---
+++ Bombus/View/Extensions/Modifiers/BombusPrimaryButtonStyle+Modifier.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct BombusPrimaryButtobnStyleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.body)
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .background(RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(Color.accentColor))
+            .padding(.horizontal, 20)
+    }
+}
+
+extension View {
+    func customButton() -> ModifiedContent<Self, BombusPrimaryButtobnStyleModifier> {
+        return modifier(BombusPrimaryButtobnStyleModifier())
+    }
+}
+
+struct BombusPrimaryButtobnStyleModifierPreview: PreviewProvider {
+
+    static var previews: some View {
+
+        Group {
+            Button(action: {}, label: {
+                Text("Button")
+            })
+            .customButton()
+        }
+    }
+}
